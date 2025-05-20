42dffe905842efe554092f0c04d707fec7ec7cd3
Catherine <catherine@raksmart.com>
2025-05-20 10:33:55 +0800

    change clear site ip
    Signed-off-by: Catherine <catherine@raksmart.com>

--- scripts/kvm/async_job/clear_sites_ip.sh
+++ scripts/kvm/async_job/clear_sites_ip.sh
@@ -24,7 +24,7 @@ if [ "$os_code" = "windows" ]; then
         while [ $j -lt $naddr ]; do
             read -d'\n' -r address < <(jq -r ".[$j]" <<<$site_addrs)
 	    read -d'\n' -r site_ip netmask  < <(ipcalc -nb $address | awk '/Address/ {print $2} /Netmask/ {print $2}')
-            virsh qemu-agent-command "$vm_ID" '{"execute":"guest-exec","arguments":{"path":"C:\\Windows\\System32\\netsh.exe","arg":["interface","ipv4","add","address","name=eth0","addr='"$site_ip"'","mask='"$netmask"'"],"capture-output":true}}'
+            virsh qemu-agent-command "$vm_ID" '{"execute":"guest-exec","arguments":{"path":"C:\\Windows\\System32\\netsh.exe","arg":["interface","ipv4","del","address","name=eth0","addr='"$site_ip"'","mask='"$netmask"'"],"capture-output":true}}'
             let j=$j+1
         done
         let i=$i+1
