47cd5800450f9dd6091a3bc6985d875a359bf3f8
Jack <jack@raksmart.com>
2025-05-28 20:16:27 +0800

    PETC-572 volume resize api
    Signed-off-by: Jack <jack@raksmart.com>

--- scripts/kvm/resize_volume_local.sh
+++ scripts/kvm/resize_volume_local.sh
@@ -3,37 +3,41 @@
 cd $(dirname $0)
 source ../cloudrc
 
-[ $# -lt 5 ] && echo "$0 <boot_vm_ID> <volume_ID> <volume_UUID> <size> <state>" && exit -1
+[ $# -lt 5 ] && echo "$0 <volume_ID> <volume_UUID> <size> <booting> <vm_ID>" && exit -1
 
-vm_ID=$1
-vol_ID=$2
-vol_UUID=$3
-vol_size=$4
-vol_state=$5
-if [ -z "$vm_ID" ] || ["$vm_ID" = "0"]; then
-    vol_path="$image_dir/inst-${vm_ID}.disk"
-else
+vol_ID=$1
+vol_UUID=$2
+vol_size=$3
+booting=$4
+vm_ID=$5
+if [ "$booting" = "false" ]; then
     vol_path="$volume_dir/volume-${vol_ID}.disk"
+else
+    vol_path="$image_dir/inst-${vm_ID}.disk"
 fi
 if [ ! -f "$vol_path" ]; then
-    echo "|:-COMMAND-:| create_volume_local '$vol_ID' '' 'error' 'volume not found'"
+    echo "|:-COMMAND-:| resize_volume '$vol_ID' 'error'"
     exit -1
 fi
 
+# if volume attached to a VM, stop the VM first
+if [ "$vm_ID" != "0" ]; then
+    ./action_vm.sh $vm_ID hard_stop
+fi
 old_size=$(qemu-img info -U $vol_path | grep 'virtual size:' | cut -d' ' -f5 | tr -d '(')
 let new_size=$vol_size*1024*1024*1024
 
 # new size must be larger than current size
 if [ "$old_size" -ge "$new_size" ]; then
-    echo "|:-COMMAND-:| create_volume_local '$vol_ID' '' 'error' 'new size must be larger than current size'"
+    echo "|:-COMMAND-:| resize_volume '$vol_ID' 'error'"
     exit -1
 fi
 
 # resize the volume
 qemu-img resize -q $vol_path "${vol_size}G"
 if [ $? -eq 0 ]; then
-    echo "|:-COMMAND-:| create_volume_local '$vol_ID' '' '$vol_state' 'success'"
+    echo "|:-COMMAND-:| resize_volume '$vol_ID' 'success'"
 else
-    echo "|:-COMMAND-:| create_volume_local '$vol_ID' '' 'error' 'resize failed'"
+    echo "|:-COMMAND-:| resize_volume '$vol_ID' 'error'"
     exit -1
 fi
