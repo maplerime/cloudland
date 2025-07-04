f699c68000a4e9b4899a5ff2a013a6b765655b34
zhesmart <zhe@raksmart.com>
2025-07-04 10:23:28 +0800

    delete useless code for this function
    Signed-off-by: zhesmart <zhe@raksmart.com>

--- scripts/kvm/apply_vm_nic.sh
+++ scripts/kvm/apply_vm_nic.sh
@@ -34,5 +34,3 @@ udevadm settle
 ./set_nic_speed.sh "$ID" "$nic_name" "$inbound" "$outbound"
 ./reapply_secgroup.sh "$vm_ip" "$vm_mac" "$allow_spoofing" "$nic_name" <<< $vlan_info
 ./set_subnet_gw.sh "$router" "$vlan" "$gateway" "$ext_vlan"
-
-echo "vm_ip=$vm_ip vm_br=$vm_br router=$router" >> "$async_job_dir/$nic_name"
