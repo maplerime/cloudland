77e1b809dc31fea2c72c186f7113fe9ad441bda0
Catherine <catherine@raksmart.com>
2025-08-23 11:31:02 +0800

    optimize operating iptables rules
    Signed-off-by: Catherine <catherine@raksmart.com>

--- scripts/kvm/system_router.sh
+++ scripts/kvm/system_router.sh
@@ -18,13 +18,13 @@ ip netns exec $router ip addr add $ext_ip dev link-$ext_vlan
 ip netns exec $router ip route add default via $gateway
 route_ip=${ext_ip%/*}
 ip netns exec $router iptables -P INPUT DROP
-ip netns exec $router iptables -D FORWARD -p tcp --dport 25 -j DROP
-ip netns exec $router iptables -I FORWARD -p tcp --dport 25 -j DROP
-ip netns exec $router iptables -D FORWARD -p tcp --dport 465 -j DROP
-ip netns exec $router iptables -I FORWARD -p tcp --dport 465 -j DROP
-ip netns exec $router iptables -D FORWARD -p tcp --dport 587 -j DROP
-ip netns exec $router iptables -I FORWARD -p tcp --dport 587 -j DROP
-ip netns exec $router iptables -t nat -S | grep "to-source $ext_ip\>"
+ip netns exec $router iptables -C FORWARD -p tcp --dport 25 -j DROP
+[ $? -ne 0 ] && ip netns exec $router iptables -I FORWARD -p tcp --dport 25 -j DROP
+ip netns exec $router iptables -C FORWARD -p tcp --dport 465 -j DROP
+[ $? -ne 0 ] && ip netns exec $router iptables -I FORWARD -p tcp --dport 465 -j DROP
+ip netns exec $router iptables -C FORWARD -p tcp --dport 587 -j DROP
+[ $? -ne 0 ] && ip netns exec $router iptables -I FORWARD -p tcp --dport 587 -j DROP
+ip netns exec $router iptables -t nat -C POSTROUTING -j SNAT --to-source $route_ip
 [ $? -ne 0 ] && ip netns exec $router iptables -t nat -A POSTROUTING -j SNAT --to-source $route_ip
 
 ip netns exec $router bash -c "echo 1 >/proc/sys/net/ipv4/ip_forward"
