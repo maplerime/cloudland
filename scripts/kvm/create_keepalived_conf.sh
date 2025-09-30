0577123d43ee7931c7a8dd22bba2fb2879fcf23e
Catherine <catherine@raksmart.com>
2025-09-30 15:05:50 +0800

    add create_keepalived_conf.sh
    Signed-off-by: Catherine <catherine@raksmart.com>

---
+++ scripts/kvm/create_keepalived_conf.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cd `dirname $0`
+source ../cloudrc
+
+[ $# -lt 8 ] && die "$0 <router> <lb_ID> <vrrp_vlan> <local_mac> <local_ip> <peer_mac> <peer_ip> <role> <virtual_ip>"
+
+router=$1
+[ "${router/router-/}" = "$router" ] && router=router-$1
+lb_ID=$2
+vrrp_vlan=$3
+local_mac=$4
+local_ip=${5%/*}
+peer_mac=$6
+peer_ip=${7%/*}
+role=$8
+virtual_ip=$9
+ext_dev=te-$1-$ext_link
+
+lb_dir=$router_dir/$router/lb-$lb_ID
+[ ! -d "$lb_dir" ] && mkdir -p $lb_dir
+cat >$lb_dir/keepalived.conf <<EOF
+vrrp_instance load_balancer_${lb_ID} {
+    state $role
+    interface lb-$vrrp_vlan
+    virtual_router_id ${lb_ID}
+    priority 10
+    advert_int 1
+
+    unicast_src_ip $local_ip
+    unicast_peer {
+        $peer_ip
+    }
+
+    authentication {
+        auth_type PASS
+        auth_pass 123456
+    }
+
+    virtual_ipaddress {
+        $virtual_ip dev $ext_dev
+    }
+}
+EOF
