a0a33b35a1114ef9f9db854c0ec93e028c98046b
bryan <bryan@raksmart.com>
2025-12-16 21:06:25 +0800

    PET-1012: add update volume qos function and web template
    Signed-off-by: bryan <bryan@raksmart.com>

--- scripts/kvm/update_volume_wds_vhost.sh
+++ scripts/kvm/update_volume_wds_vhost.sh
@@ -9,30 +9,19 @@ vol_ID=$1
 wds_vol_ID=$2
 iops_limit=$3
 bps_limit=$4
-state='pending'
 
 get_wds_token
 
-update_ret=$(wds_curl PUT "api/v2/sync/block/volumes/$wds_vol_ID/qos" "\{"iops_limit\": $iops_limit, "bps_limit\": $bps_limit\}")
+update_ret=$(wds_curl PUT "api/v2/sync/block/volumes/$wds_vol_ID/qos" "{\"iops_limit\": $iops_limit, \"bps_limit\": $bps_limit}")
 log_debug $vol_ID "update volume qos: $update_ret"
 
 
-read -d'\n' -r task_id ret_code message < <(jq -r ".task_id, .ret_code, .message" <<<$update_ret)
+read -d'\n' -r ret_code message < <(jq -r ".ret_code, .message" <<<$update_ret)
 if [ "$ret_code" != "0" ]; then
     echo "|:-COMMAND-:| $(basename $0) '$vol_ID' 'error' 'failed to update volume qos: $update_ret'"
     exit -1
 fi
-log_debug $vol_ID "update volume qos task $task_id created"
-for i in {1..720}; do
-        st=$(wds_curl GET "api/v2/sync/block/volumes/tasks/$task_id" | jq -r .task.state)
-        [ "$st" = "TASK_COMPLETE" ] && state=success && break
-        [ "$st" = "TASK_FAILED" ] && state=failed && break
-    sleep 10
-done
+log_debug $vol_ID "update volume qos success"
 
-if [ "$state" != "success" ]; then
-    echo "|:-COMMAND-:| $(basename $0) '$vol_ID' 'error' 'failed to update volume qos: $update_ret'"
-    exit -1
-fi
 
-echo "|:-COMMAND-:| $(basename $0) '$vol_ID' '$state' 'success'"
+echo "|:-COMMAND-:| $(basename $0) '$vol_ID' 'success' 'success'"
