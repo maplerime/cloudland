39968e94561ea250c41985a9eea6e5dff577d23e
Jack <jack@raksmart.com>
2026-01-15 14:40:51 +0800

    PET-1108: fix: prevent disk size overflow in rcNeeded calculation
    Signed-off-by: Jack <jack@raksmart.com>

--- web/src/routes/migration.go
+++ web/src/routes/migration.go
@@ -140,7 +140,7 @@ func (a *MigrationAdmin) Create(ctx context.Context, name string, instances []*m
 				err = nil
 				continue
 			}
-			rcNeeded := fmt.Sprintf("cpu=%d memory=%d disk=%d network=%d", instance.Cpu, instance.Memory*1024, instance.Disk*1024*1024, 0)
+			rcNeeded := fmt.Sprintf("cpu=%d memory=%d disk=%d network=%d", instance.Cpu, instance.Memory*1024, int64(instance.Disk)*1024*1024, 0)
 			control = "select=" + hyperGroup + rcNeeded
 		}
 		err = db.Model(instance).Update("status", model.InstanceStatusMigrating).Error
