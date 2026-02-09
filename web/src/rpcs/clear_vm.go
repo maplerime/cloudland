007a49d80342c170a7f3f44d99654b2a6502b30c
wusit <jack@raksmart.com>
2026-02-09 16:00:00 +0800

    PET-1228:  Adjust the order of delete and update (#333)
    * PET-1228: change (save + delete) to updates
    
    Signed-off-by: Jack <jack@raksmart.com>
    
    * PET-1228: change (save + delete) to updates
    
    Signed-off-by: Jack <jack@raksmart.com>
    
    * PET-1228: Adjust the order of delete and update
    
    Signed-off-by: Jack <jack@raksmart.com>
    
    * PET-1228: Adjust the order of delete and update
    
    Signed-off-by: Jack <jack@raksmart.com>
    
    ---------
    
    Signed-off-by: Jack <jack@raksmart.com>

--- web/src/rpcs/clear_vm.go
+++ web/src/rpcs/clear_vm.go
@@ -14,8 +14,6 @@ import (
 
 	. "web/src/common"
 	"web/src/model"
-
-	"github.com/jinzhu/gorm"
 )
 
 func init() {
@@ -165,17 +163,20 @@ func ClearVM(ctx context.Context, args []string) (status string, err error) {
 		logger.Error("Failed to delete interfaces", err)
 		return
 	}
-
-	// DELETE instance
-	err = db.Model(&model.Instance{}).Where("id = ?", instance.ID).Updates(map[string]interface{}{
-		"hostname":   fmt.Sprintf("%s-%d", instance.Hostname, instance.CreatedAt.Unix()),
-		"status":     model.InstanceStatusDeleted,
-		"reason":     reason,
-		"deleted_at": gorm.NowFunc(),
-	}).Error
-	if err != nil {
-		logger.Error("failed to delete instance with name update", err)
+	if err = db.Delete(instance).Error; err != nil {
+		logger.Error("Failed to delete instance, %v", err)
 		return
 	}
+	// Unscoped update
+	err = db.Model(&model.Instance{}).Unscoped().Where("id = ?", instance.ID).Updates(map[string]interface{}{
+		"hostname": fmt.Sprintf("%s-%d", instance.Hostname, instance.CreatedAt.Unix()),
+		"status":   model.InstanceStatusDeleted,
+		"reason":   reason,
+	}).Error
+	if err != nil {
+		logger.Error("Failed to update instance, %v", err)
+		return
+	}
+
 	return
 }
