f094f833b2c522185497adab19b23ab0fd911d45
Jack <jack@raksmart.com>
2026-03-06 12:18:30 +0800

    PET-1227: swap the deletion order of address and interface
    Signed-off-by: Jack <jack@raksmart.com>

--- web/src/rpcs/clear_vm.go
+++ web/src/rpcs/clear_vm.go
@@ -78,6 +78,20 @@ func deleteInterfaces(ctx context.Context, instance *model.Instance, vrrpInstanc
 		i++
 	}
 	for _, iface := range interfaces {
+		// Delete/update interface first, then deallocate address (matching create path lock order: interface -> address)
+		if iface.FloatingIp == 0 {
+			err = db.Delete(iface).Error
+			if err != nil {
+				logger.Error("Failed to delete interface", err)
+				return
+			}
+		} else {
+			err = db.Model(iface).Update(map[string]interface{}{"instance": 0, "primary_if": false, "name": "fip", "inbound": 0, "outbound": 0, "allow_spoofing": false}).Error
+			if err != nil {
+				logger.Error("Failed to Update interface, %v", err)
+				return
+			}
+		}
 		if iface.FloatingIp == 0 {
 			err = db.Model(&model.Address{}).Where("interface = ?", iface.ID).Update(map[string]interface{}{"allocated": false, "interface": 0}).Error
 			if err != nil {
@@ -94,19 +108,6 @@ func deleteInterfaces(ctx context.Context, instance *model.Instance, vrrpInstanc
 		if err != nil {
 			logger.Error("Failed to Update addresses, %v", err)
 			return
-		}
-		if iface.FloatingIp == 0 {
-			err = db.Delete(iface).Error
-			if err != nil {
-				logger.Error("Failed to delete interface", err)
-				return
-			}
-		} else {
-			err = db.Model(iface).Update(map[string]interface{}{"instance": 0, "primary_if": false, "name": "fip", "inbound": 0, "outbound": 0, "allow_spoofing": false}).Error
-			if err != nil {
-				logger.Error("Failed to Update addresses, %v", err)
-				return
-			}
 		}
 		err = db.Model(&model.Subnet{}).Where("interface = ?", iface.ID).Updates(map[string]interface{}{
 			"interface": 0}).Error
