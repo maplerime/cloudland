d0d655a678b36c8cf59e00098cc1ad800d807680
dragon <dragon@raksmart.com>
2026-06-09 12:02:30 +0800

    fixed the missing target_node issue
    Signed-off-by: dragon <dragon@raksmart.com>

--- web/src/callback/metadata.go
+++ web/src/callback/metadata.go
@@ -313,7 +313,7 @@ func extractInstanceInfo(db *gorm.DB, resourceID int64, unscoped bool, args []st
 					Hostname string
 				}
 				// source hyper hostname
-				if mr.SourceHyper > 0 {
+				if mr.SourceHyper >= 0 {
 					sn := &hyperName{}
 					if err := db.Raw(`SELECT hostname FROM hypers WHERE hostid = ? LIMIT 1`, mr.SourceHyper).Scan(sn).Error; err != nil {
 						logger.Warningf("[%s] extractInstanceInfo: source hyper query failed hostid=%d: %v", traceID, mr.SourceHyper, err)
@@ -322,7 +322,7 @@ func extractInstanceInfo(db *gorm.DB, resourceID int64, unscoped bool, args []st
 					}
 				}
 				// target hyper hostname
-				if mr.TargetHyper > 0 {
+				if mr.TargetHyper >= 0 {
 					tn := &hyperName{}
 					if err := db.Raw(`SELECT hostname FROM hypers WHERE hostid = ? LIMIT 1`, mr.TargetHyper).Scan(tn).Error; err != nil {
 						logger.Warningf("[%s] extractInstanceInfo: target hyper query failed hostid=%d: %v", traceID, mr.TargetHyper, err)
