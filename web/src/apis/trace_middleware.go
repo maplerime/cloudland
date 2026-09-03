4aa22de20f776e3b73e909d14c157ba5e16e1b57
bryan <bryan@raksmart.com>
2026-09-03 11:22:44 +0800

    PET-1901: X-Request-ID and trace ID
    Signed-off-by: bryan <bryan@raksmart.com>

---
+++ web/src/apis/trace_middleware.go
@@ -0,0 +1,30 @@
+/*
+Copyright <holder> All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package apis
+
+import (
+	rlog "web/src/utils/log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+// TraceMiddleware reads X-Request-ID from the request header as the Trace ID.
+// If absent, generates a new UUID. Injects it into the request context and
+// echoes it back via X-Trace-ID response header.
+func TraceMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		traceID := c.GetHeader("X-Request-ID")
+		if traceID == "" {
+			traceID = uuid.New().String()
+		}
+		ctx := rlog.SetTraceID(c.Request.Context(), traceID)
+		c.Request = c.Request.WithContext(ctx)
+		c.Header("X-Trace-ID", traceID)
+		c.Next()
+	}
+}
