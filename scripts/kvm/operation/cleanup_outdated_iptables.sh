3808d77400abe924364a41aa76617882ab24909d
Catherine <catherine@raksmart.com>
2026-09-08 15:35:10 +0800

    PET-1919: move iptables -A INPUT -s 10.0.0.0/8 -j ACCEPT the first
    Signed-off-by: Catherine <catherine@raksmart.com>

--- scripts/kvm/operation/cleanup_outdated_iptables.sh
+++ scripts/kvm/operation/cleanup_outdated_iptables.sh
@@ -18,15 +18,27 @@ done
 
 lock_file="$run_dir/iptables.lock"
 exec 200>>"$lock_file"
+ln=$(iptables -n -L FORWARD --line-numbers | grep 'state RELATED,ESTABLISHED' | tail -1 | cut -d' ' -f1)
+[ "$ln" != 1 ] && iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 for i in {1..10}; do
     ln=$(iptables -n -L FORWARD --line-numbers | grep 'state RELATED,ESTABLISHED' | tail -1 | cut -d' ' -f1)
     [ "$ln" = 1 ] && break
-    iptables -D FORWARD $ln
-    iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+    [ -n "$ln" ] && iptables -D FORWARD $ln
 done
 iptables -N secgroup-chain && iptables -A secgroup-chain -j ACCEPT
 ln=$(iptables -n -L secgroup-chain --line-numbers | grep 'ACCEPT' | head -1 | cut -d' ' -f1)
 if [ "$ln" = 1 ]; then
     iptables -A secgroup-chain -j ACCEPT && iptables -D secgroup-chain -j ACCEPT
 fi
+
+# Ensure the internal-network allow rule sits at the very top of INPUT.
+# Same approach as the FORWARD loop above: only act when it is not already at
+# line 1 - delete the misplaced/duplicate copy and re-insert it at the head.
+ln=$(iptables -n -L INPUT --line-numbers | grep 'ACCEPT .*10.0.0.0/8' | tail -1 | cut -d' ' -f1)
+[ "$ln" != 1 ] && iptables -I INPUT -s 10.0.0.0/8 -j ACCEPT
+for i in {1..10}; do
+    ln=$(iptables -n -L INPUT --line-numbers | grep 'ACCEPT .*10.0.0.0/8' | tail -1 | cut -d' ' -f1)
+    [ "$ln" = 1 ] && break
+    [ -n "$ln" ] && iptables -D INPUT $ln
+done
 flock -u 200
